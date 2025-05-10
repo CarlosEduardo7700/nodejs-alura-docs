63c1d80917961db14b5fe4401a7935b911aff6a3
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-05-09 21:09:40 -0300

    feat: Configura o editor de texto para emitir eventos para o socket

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const editorDeTexto = document.getElementById("editor-texto");
+
+editorDeTexto.addEventListener("keyup", () => {
+    socket.emit("texto_do_editor", editorDeTexto.value);
+});
